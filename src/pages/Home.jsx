9c3d20233559affee53c5014f676def126b5491d
rajmanvi17 <your-github-email>
2026-04-26 21:30:31 +0530

    Added caching headers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,7 +35,7 @@ export default function Home() {
             objectFit: 'cover', zIndex: 0,
           }}
         >
-          <source src="/bg.mp4" type="video/mp4" />
+          <source src="/bg1.mp4" type="video/mp4" />
         </video>
 
         <div style={{
